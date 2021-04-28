acd2e51a8928fe961d5cd9b84f581b2d1831cc92
Ben Allred <skuttleman@gmail.com>
2021-04-28 06:49:37 -0600

    print failures when running browser tests

--- test/cljc/test/browser_runner.clj
+++ test/cljc/test/browser_runner.clj
@@ -2,14 +2,17 @@
   (:require
     [clojure.string :as string]
     [com.ben-allred.audiophile.api.utils.ring :as ring]
+    [com.ben-allred.audiophile.common.utils.maps :as maps]
     [immutant.web :as web])
   (:import
-    (org.openqa.selenium By)
+    (org.openqa.selenium By SearchContext WebElement)
     (org.openqa.selenium.firefox FirefoxDriver)))
 
-(def ^:const ANSI_RESET "\u001B[0m")
-(def ^:const ANSI_RED "\u001B[31m")
-(def ^:const ANSI_GREEN "\u001B[32m")
+(def ^:const RESET "\u001B[0m")
+(def ^:const RED "\u001B[31m")
+(def ^:const GREEN "\u001B[32m")
+(def ^:const CYAN "\u001B[36m")
+(def ^:const GOLD "\u001B[33;1m")
 
 (defn ready? [driver]
   (let [xs (reduce (fn [bldr el]
@@ -18,25 +21,63 @@
                    (.findElements driver (By/id "summary")))]
     (string/includes? (str xs) "Totals")))
 
+(defn print-failures! [failures]
+  (doseq [{:keys [ns var line context expected actual]} failures]
+    (println "")
+    (println (str RED "FAILURE" RESET))
+    (println (str ns var))
+    (println (str "at " CYAN line RESET))
+    (println (str "  " context))
+    (println (str "    EXPECTED: " expected))
+    (println (str "    ACTUAL:   " GOLD actual RESET))))
+
+(defn ^:private by-css [^SearchContext element selector]
+  (.findElements element (By/cssSelector selector)))
+
+(defn ^:private texts [elements]
+  (map #(.getText %) elements))
+
+(defn ^:private text [elements]
+  (string/join "\n" (texts elements)))
+
 (defn -main [& _]
   (let [env (System/getenv)
-        server-port (Long/parseLong (or (get env "UI_TEST_PORT") "8080"))
-        _ (web/run #(or (ring/resource-request % "private")
-                        {:status 204})
-                   {:port server-port :host "0.0.0.0"})
-        driver (FirefoxDriver.)]
-    (println "Running CLJS tests…")
-    (.get driver (str "http://localhost:" server-port "/index.html"))
-    (while (not (ready? driver))
-      (Thread/sleep 1))
-    (let [tests (count (.findElements driver (By/className "test-var")))
-          passes (count (.findElements driver (By/className "test-passing")))
-          fails (count (.findElements driver (By/className "test-fail")))
-          [color exit-code] (if (zero? fails)
-                              [ANSI_GREEN 0]
-                              [ANSI_RED 1])]
-      (println "")
-      (println (str color tests " tests. " passes " passed. " fails " failed." ANSI_RESET))
-
-      (.quit driver)
-      (System/exit exit-code))))
+        exit-code (volatile! 1)
+        server-port (Long/parseLong (or (get env "UI_TEST_PORT") "8080"))]
+    (try
+      (let [server (web/run #(or (ring/resource-request % "private")
+                                 {:status 204})
+                            {:port server-port :host "0.0.0.0"})
+            driver (FirefoxDriver.)]
+        (try
+          (println "Running CLJS tests…")
+          (.get driver (str "http://localhost:" server-port "/index.html"))
+          (while (not (ready? driver))
+            (Thread/sleep 1))
+          (let [tests (count (by-css driver ".test-var"))
+                passes (count (by-css driver ".test-passing"))
+                fails (count (by-css driver ".test-fail"))
+                failures (for [^WebElement el (by-css driver ".test-ns.has-failures")
+                               :let [ns (text (by-css el "h2"))]
+                               ^WebElement el (by-css el ".test-var.has-failures")
+                               :let [line (text (by-css el ".test-var-line"))
+                                     var (-> (by-css el ".var-header")
+                                             text
+                                             (string/replace line "")
+                                             string/trim)]
+                               ^WebElement el (by-css el ".test-fail")
+                               :let [context (text (by-css el ".contexts"))
+                                     [expected actual] (texts (by-css el "pre > code"))]]
+                           (maps/->m ns var line context expected actual))
+                [color code] (if (zero? fails)
+                               [GREEN 0]
+                               [RED 1])]
+            (vreset! exit-code code)
+            (print-failures! failures)
+            (println "")
+            (println (str color tests " tests. " passes " passed. " fails " failed." RESET)))
+          (finally
+            (.quit driver)
+            (web/stop server))))
+      (finally
+        (System/exit @exit-code)))))
