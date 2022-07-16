33ac89029cc5c4a0f4872eed2ed79ee2cb100ac2
Ben Allred <skuttleman@gmail.com>
2022-07-16 13:26:41 -0600

    migration for team-invitations

---
+++ resources/db/migrations/20220716170549-team-invitations.down.sql
@@ -0,0 +1,9 @@
+ALTER TABLE team_invitations
+    DROP CONSTRAINT team_invitations_valid_email;
+--;;
+
+DROP TABLE team_invitations;
+--;;
+
+DROP TYPE TEAM_INVITATION_STATUS;
+--;;
---
+++ resources/db/migrations/20220716170549-team-invitations.up.sql
@@ -0,0 +1,16 @@
+CREATE TYPE TEAM_INVITATION_STATUS AS ENUM ('PENDING', 'ACCEPTED', 'REVOKED');
+--;;
+
+CREATE TABLE team_invitations (
+    team_id UUID REFERENCES teams NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    status TEAM_INVITATION_STATUS NOT NULL DEFAULT 'PENDING'::TEAM_INVITATION_STATUS,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+
+    PRIMARY KEY(team_id, email)
+);
+--;;
+
+ALTER TABLE team_invitations
+    ADD CONSTRAINT team_invitations_valid_email CHECK (email ~ '^[a-z\-\+_0-9\.]+@[a-z\-\+_0-9]+\.[a-z\-\+_0-9\.]+$');
+--;;
